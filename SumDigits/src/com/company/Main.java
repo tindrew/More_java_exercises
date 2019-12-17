1ebf3ad3e369c494731d7bddcf9faeb534f8120e
Andrew Martin <andevr@pop-os.localdomain>
2019-12-17 07:19:43 -0600

    Added SumDigits

---
+++ SumDigits/src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        sumDigits(125);
+        sumDigits(4);
+        sumDigits(32123);
+        sumDigits(-125);
+    }
+
+    public static int sumDigits (int number) {
+        if(number < 10) {
+            System.out.println("negative number");
+            return -1;
+            }
+
+            int num = 0;
+            while (number > 0) {
+                num += (number % 10);
+                number = number / 10;
+
+
+        }
+        System.out.println(num);
+        return num;
+    }
+
+
+}
